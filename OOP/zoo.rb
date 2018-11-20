ec8f117250e0cac2808138af98e8a222402896fd
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-20 23:17:50 +0000

    Created the Zoo class

---
+++ OOP/zoo.rb
@@ -0,0 +1,24 @@
+class Zoo
+
+  def animals
+    puts "We have Birds, Reptiles and Fishes in this Zoo park"
+  end
+
+  def staff
+    puts "Our staffs are dilligent and dedicated to working here"
+  end
+
+  def visitors
+    puts "You are welcome to the London Zoo"
+  end
+
+  def giftShop
+    puts "We sell various artefacts"
+  end
+end
+
+# my = Zoo.new
+# my.animals
+# my.staff
+# my.visitors
+# my.giftShop
