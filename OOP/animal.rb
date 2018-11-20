ba34f2346365300953827c6a99f26b88494b6bd0
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-20 23:15:59 +0000

    Added a Zoo class

---
+++ OOP/animal.rb
@@ -0,0 +1,35 @@
+require_relative "./zoo.rb"
+
+class Animal < Zoo
+  def self.traits
+    puts "Animals can breathe, eat, drink, speak and procreate"
+  end
+
+  def breathe
+    puts "In and Out"
+  end
+
+  def procreate
+    puts "An animal can procreate"
+  end
+
+  def eat
+    puts "An animal can eat"
+  end
+
+  def drink
+    puts "gulp gulp gulp!!!"
+  end
+
+  def speak
+    puts "Hello I am an animal"
+  end
+
+end
+
+# #using instance method
+# animal = Animal.new
+# animal.breathe
+#
+# # #using self (class method)
+# Animal.traits
